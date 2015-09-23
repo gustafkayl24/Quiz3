36ecff20f027c004c6ce16f32d8ed7a1a76f3abd
Kayla <Kayla@128.4.100.208>
2015-09-23 19:07:21 -0400

    initial

--- PS3_Gustafson/src/Account.java
+++ PS3_Gustafson/src/Account.java
@@ -1,42 +1,67 @@
 
 import java.util.Date;
 
-public class Account 
-{
+public class Account {
 	private int ID;
 	private double Balance;
 	private double annualInterestRate;
-	private java.util.Date dateCreated;
-	public Account(int ID, double Balance, double annualInterestRate, Date dateCreated){
+	private Date dateCreated;
+
+	public Account(int ID, double Balance) {
+		super();
 		this.Balance = Balance;
 		this.ID = ID;
-		this.annualInterestRate = annualInterestRate;
-		this.dateCreated = java.util.Date();
+		// this.annualInterestRate = annualInterestRate;
+		// this.dateCreated = dateCreated;
 	}
-	public Account(){
+
+	public Account() {
 		this.Balance = 0;
 		this.ID = 0;
-		this.annualInterestRate = 0;
+		// this.annualInterestRate = 0;
 	}
 
-public void deposit(double amount){
-	amount += Balance;
-}
-public void withdraw(double amount){
-	if (amount <= Balance)
-	{
-		Balance -= amount;
+	public int getID() {
+		return ID;
 	}
-	else
-	{
-		double needs = amount - Balance;
-		throw new InsufficientFundsException(needs);
-	}
-}
 
-public double getMonthlyInterestRate(double monthlyRate){
-	monthlyRate = annualInterestRate/12;
-	return monthlyRate;
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public double getBalance() {
+		return Balance;
+	}
+
+	public void setBalance(double balance) {
+		this.Balance = balance;
+	}
+
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+
+	public void deposit(double amount) {
+		Balance += amount;
+	}
+
+	public void withdraw(double amount) throws InsufficientFundsException {
+		if (amount <= Balance) {
+			Balance -= amount;
+		} 
+		else 
+		{
+			double needs = amount - Balance;
+			throw new InsufficientFundsException(needs);
+		}
+	}
+
+	public double getMonthlyInterestRate(double monthlyRate) {
+		monthlyRate = annualInterestRate / 12;
+		return monthlyRate;
+	}
 }
-}
-	
--- PS3_Gustafson/src/AccountTest.java
+++ PS3_Gustafson/src/AccountTest.java
@@ -1,26 +1,35 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AccountTest {
 
-	@Test
-	public void test() {
-		 Account c = new Account(20000);
-	     
-	      try
-	      {
-	    	 System.out.println("You are depositing $3,000...");
-		     c.deposit(3000.00);
-	         System.out.println("You are withdrawing $2500...");
-	         c.withdraw(2500.00);
-	      }catch(InsufficientFundsException e)
-	      {
-	         System.out.println("Sorry, but your request could not be complete due to not enough funds"
-	              + e.getAmount());
-	         	  e.printStackTrace();
-	      }
-	         
+	Account checking;
+@Before
+public void setUp()throws Exception {
+	checking = new Account(1122,20000);
+}
+@After
+public void tearDown()throws Exception {
+	checking = null;
+}	
+
+@Test
+public final void testWithdraw() throws InsufficientFundsException 
+	{
+	checking.withdraw(2500);
+	System.out.println(checking.getBalance());
+	assertTrue("The final is not $17500.00",(double)checking.getBalance()==(double)17500);
+    
+}
+
+@Test
+public final void testDeposit(){
+
+		checking.deposit(3000);
+		assertTrue("The final is not $23,000.00", checking.getBalance()==23000);
 	}
 
 }
